6f72f9c4a08374eded82407f605c05bb86553287
Your Name <xbotraid@gmail.com>
2025-07-16 03:50:23 +0200

    Update site title from 'Mermaid Documentation' to 'Merlin Guide'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Mermaid Documentation',
+  title: 'Merlin Guide',
   tagline: 'Bridging Code and Diagrams for Better Understanding',
   favicon: 'img/favicon.ico',
 
